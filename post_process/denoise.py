325b87068ad44d430488c1f8f92a0be09467d1f2
Thomas Marrinan <thomas.j.marrinan@gmail.com>
2023-03-31 10:50:40 -0500

    update denoise script

--- post_process/denoise.py
+++ post_process/denoise.py
@@ -4,23 +4,31 @@ import numpy as np
 
 
 def main():
-    exr = OpenEXR.InputFile('../public/data/office_dasp_nodenoise_0.33_4k.exr')
+    #exr = OpenEXR.InputFile('../public/data/office_dasp_nodenoise_0.33_4k.exr')
+    exr = OpenEXR.InputFile('../public/data/office_dasp.exr')
     
     header = exr.header()
     dw = header['dataWindow']
     isize = (dw.max.y - dw.min.y + 1, dw.max.x - dw.min.x + 1)
     
     color_channels = {}
+    depth_channel = {}
     for c in header['channels']:
         channel_name = ''
         color = ''
         if c == 'R' or c == 'G' or c == 'B':
             channel_name = 'DEFAULT'
             color = c
-        if len(c) > 2 and (c[-2:] == '.R' or c[-2:] == '.G' or c[-2:] == '.B'):
-            channel_name = c[:-2]
+        elif len(c) > 8 and c[:6] == 'Image.' and (c[-2:] == '.R' or c[-2:] == '.G' or c[-2:] == '.B'):
+            channel_name = c[6:-2]
             color = c[-1:]
-        if color == 'R' or color == 'G' or color == 'B':
+        elif c == 'Z':
+            channel_name = 'DEFAULT'
+            color = c
+        elif len(c) > 8 and c[:6] == 'Depth.' and c[-2:] == '.V':
+            channel_name = c[6:-2]
+            color = 'Z'
+        if color == 'R' or color == 'G' or color == 'B' or color == 'Z':
             px_array = None
             if header['channels'][c].type == Imath.PixelType(Imath.PixelType.HALF):
                 px_array = np.frombuffer(exr.channel(c, Imath.PixelType(Imath.PixelType.HALF)), dtype=np.float16)
@@ -31,12 +39,32 @@ def main():
             else:
                 print('OpenEXR UINT pixel type not supported')
             
-            if not channel_name in color_channels:
-                color_channels[channel_name] = {'R': None, 'G': None, 'B': None}
-            
-            color_channels[channel_name][color] = px_array
+            if color == 'Z':
+                depth_channel[channel_name] = px_array
+            else:
+                if not channel_name in color_channels:
+                    color_channels[channel_name] = {'R': None, 'G': None, 'B': None}
+                
+                color_channels[channel_name][color] = px_array
+   
     for ch in color_channels:
         print(ch)
-        print(color_channels[ch])
+        depthAwareDenoise(color_channels[ch], depth_channel[ch], isize)
+
+def depthAwareDenoise(color_data, depth_data, size):
+    for y in range(size[0]):
+        for x in range(size[1]):
+            rgb = getColor(color_data, x, y)
+            depth = getDepth(depth_data, x, y)
+            # TODO: get 8 surrounding pixels as well
+            if x == 0 and y == 0:
+                print(rgb, depth)
+            
+
+def getColor(color_data, x, y):
+    return [color_data['R'][y][x], color_data['G'][y][x], color_data['B'][y][x]]
+    
+def getDepth(depth_data, x, y):
+    return depth_data[y][x]
 
 main()
